86e73d047b18771927737ba53afc4b829aa6d80f
nizan153 <nizan153@gmail.com>
2019-05-28 12:56:56 +0300

    update chain and range

--- chain.hpp
+++ chain.hpp
@@ -1,3 +1,51 @@
+// #pragma once
+// #include<iostream>
+// using namespace std;
+
+// namespace itertools{
+//     template<typename T1,typename T2>
+
+//     class chain{
+//         private:
+//         T1 t1;
+//         T2 t2;
+        
+//         public:
+//         chain(const T1& begins,const T2& ends):t1(begins),t2(ends){ }
+        
+//         class iterator {
+//             public: 
+//                 decltype(t1.begin()) t1Begin;
+//                 decltype(t1.end()) t1End;
+//                 decltype(t2.begin()) t2Begin;
+//                 decltype(t2.end()) t2End;
+
+//                 iterator(T1& a, T2& b) : t1Begin(a.begin()), t1End(a.end()), t2Begin(b.begin()), t2End(b.end()) { }
+
+//                 iterator(T1& a, T2& b, bool isEnd) : t1Begin(a.end()), t1End(a.end()), t2Begin(b.end()), t2End(b.end()) { }
+
+//                 const auto operator*() {
+//                     if (t1Begin != t1End) { return *t1Begin; }
+//                     return *t2Begin;
+//                 }
+
+//                 const iterator& operator++() {
+//                     if(t1Begin != t1End) { ++t1Begin; }
+//                     else { ++t2Begin; }
+//                     return *this;
+//                 }
+
+//                 bool operator!=(const iterator& it) {
+//                     if (t1Begin != it.t1Begin || t2Begin != it.t2Begin) { return true; }
+//                     return false;
+//                 }
+//         };
+
+//         iterator begin() const { return iterator(t1, t2); }
+//         iterator end() const { return iterator(t1, t2, true); }
+//     };
+// }
+
 #pragma once
 #include<iostream>
 using namespace std;
@@ -13,36 +61,36 @@ namespace itertools{
         public:
         chain(const T1& begins,const T2& ends):t1(begins),t2(ends){ }
         
-        class iterator {
+        class const_iterator {
             public: 
-                decltype(t1.begin()) t1Begin;
-                decltype(t1.end()) t1End;
-                decltype(t2.begin()) t2Begin;
-                decltype(t2.end()) t2End;
+                typename T1::const_iterator t1Begin;
+                typename T1::const_iterator t1End;
+                typename T2::const_iterator t2Begin;
+                typename T2::const_iterator t2End;
 
-                iterator(T1& a, T2& b) : t1Begin(a.begin()), t1End(a.end()), t2Begin(b.begin()), t2End(b.end()) { }
+                const_iterator(const typename T1::const_iterator& it1Begin, const typename T1::const_iterator& it1End
+                ,const typename T2::const_iterator& it2Begin, const typename T2::const_iterator& it2End) :
+                 t1Begin(it1Begin), t1End(it1End), t2Begin(it2Begin), t2End(it2End) { }
 
-                iterator(T1& a, T2& b, bool isEnd) : t1Begin(a.end()), t1End(a.end()), t2Begin(b.end()), t2End(b.end()) { }
-
-                auto operator*() {
+                const auto operator*() {
                     if (t1Begin != t1End) { return *t1Begin; }
                     return *t2Begin;
                 }
 
-                iterator& operator++() {
+                const const_iterator& operator++() {
                     if(t1Begin != t1End) { ++t1Begin; }
                     else { ++t2Begin; }
                     return *this;
                 }
 
-                bool operator!=(const iterator& it) {
+                bool operator!=(const const_iterator& it) {
                     if (t1Begin != it.t1Begin || t2Begin != it.t2Begin) { return true; }
                     return false;
                 }
         };
 
-        auto begin() { return iterator(t1, t2); }
-        auto end() { return iterator(t1, t2, true); }
+        const_iterator begin() const { return const_iterator(t1.begin(), t1.end(), t2.begin(), t2.end()); }
+        const_iterator end() const { return const_iterator(t1.end(), t1.end(), t2.end(), t2.end()); }
     };
 }
 
--- range.hpp
+++ range.hpp
@@ -1,5 +1,208 @@
+// // #pragma once
+// // //#include <iostream>
+
+// // namespace itertools{
+    
+// //     template<typename T>
+
+// //     class range
+// //     {
+// //     private:
+// //         T r_begin;
+// //         T r_end;
+// //     public:
+
+// //         range(const T begins,const T ends){
+// //             r_begin=begins;
+// //             r_end=ends;
+// //         }
+// //         int length () const{
+// //             return r_end.length()-r_begin.length();
+// //         }
+// //        // range<T>(const range& ran):r_begin(ran.r_begin),r_end(ran.r_end){}
+
+// //         class iterator{
+// //             private:
+// //             T it;
+
+// //             public:
+// //             iterator(T iter = nullptr): it(iter) {}
+// //             //iterator(const iterator &other):it(other.it){}
+
+// //             T operator*()const{
+// //                 return  it;
+// //             }
+// //             //++i prefix
+// //             iterator& operator++(){
+// //                 it++;
+// //                 return *this;
+// //             }
+// //             //i++ postfix
+// //             const iterator operator ++(int){
+// //                 iterator temp =*this;
+// //                 it++;
+// //                 return temp;
+// //             }
+// //             bool operator==(const iterator& other){
+// //                 return it==other.it;
+// //             }
+// //             bool operator!=(const iterator& other){
+// //                 return it!=other.it; 
+// //             }
+            
+// //             // T* operator-> ()const{
+// //             //     return &(r_it);
+// //             // }
+// //          //   friend ostream& operator<<(ostream& os,const iterator& it);
+
+// //         };//end of iterator class
+       
+// //         public:
+// //         iterator begin()const{
+// //          return iterator{r_begin};
+// //         }
+// //         iterator end()const{
+// //             return iterator{r_end};
+// //         }
+// //     };   
+        
+// // }//;
+
+// #pragma once
+// #include <iostream>
+
+// namespace itertools{
+    
+//     template<typename T>
+
+//     class range
+//     {
+//     private:
+//         T r_begin;
+//         T r_end;
+//     public:
+
+//         range(const T& begins,const T& ends){
+//             r_begin=begins;
+//             r_end=ends;
+//         }
+//         // int length () const{
+//         //     return r_end.length()-r_begin.length();
+//         // }
+//        // range<T>(const range& ran):r_begin(ran.r_begin),r_end(ran.r_end){}
+
+//         class iterator{
+//             private:
+//             T it;
+
+//             public:
+//             iterator(T iter = nullptr): it(iter) {}
+//             //iterator(const iterator &other):it(other.it){}
+
+//             const T operator*()const{
+//                 return  it;
+//             }
+//             //++i prefix
+//             iterator& operator++(){
+//                 it++;
+//                 return *this;
+//             }
+//             //i++ postfix
+//             const iterator operator ++(int){
+//                 iterator temp =*this;
+//                 it++;
+//                 return temp;
+//             }
+//             bool operator==(const iterator& other) const {
+//                 return it==other.it;
+//             }
+//             bool operator!=(const iterator& other) const {
+//                 return it!=other.it; 
+//             }
+//         };//end of iterator class
+       
+//         public:
+//         iterator begin() const{
+//          return iterator(r_begin);
+//         }
+//         iterator end() const{
+//             return iterator(r_end);
+//         }
+//     };
+// }
+
+// #pragma once
+// //#include <iostream>
+
+// namespace itertools{
+    
+//     template<typename T>
+
+//     class range
+//     {
+//     private:
+//         T r_begin;
+//         T r_end;
+//     public:
+
+//         range(const T begins,const T ends){
+//             r_begin=begins;
+//             r_end=ends;
+//         }
+//         int length () const{
+//             return r_end.length()-r_begin.length();
+//         }
+//        // range<T>(const range& ran):r_begin(ran.r_begin),r_end(ran.r_end){}
+
+//         class iterator{
+//             private:
+//             T it;
+
+//             public:
+//             iterator(T iter = nullptr): it(iter) {}
+//             //iterator(const iterator &other):it(other.it){}
+
+//             T operator*()const{
+//                 return  it;
+//             }
+//             //++i prefix
+//             iterator& operator++(){
+//                 it++;
+//                 return *this;
+//             }
+//             //i++ postfix
+//             const iterator operator ++(int){
+//                 iterator temp =*this;
+//                 it++;
+//                 return temp;
+//             }
+//             bool operator==(const iterator& other){
+//                 return it==other.it;
+//             }
+//             bool operator!=(const iterator& other){
+//                 return it!=other.it; 
+//             }
+            
+//             // T* operator-> ()const{
+//             //     return &(r_it);
+//             // }
+//          //   friend ostream& operator<<(ostream& os,const iterator& it);
+
+//         };//end of iterator class
+       
+//         public:
+//         iterator begin()const{
+//          return iterator{r_begin};
+//         }
+//         iterator end()const{
+//             return iterator{r_end};
+//         }
+//     };   
+        
+// }//;
+
 #pragma once
-//#include <iostream>
+#include <iostream>
 
 namespace itertools{
     
@@ -12,59 +215,47 @@ namespace itertools{
         T r_end;
     public:
 
-        range(const T begins,const T ends){
+        range(const T& begins,const T& ends){
             r_begin=begins;
             r_end=ends;
         }
-        int length () const{
-            return r_end.length()-r_begin.length();
-        }
-       // range<T>(const range& ran):r_begin(ran.r_begin),r_end(ran.r_end){}
 
-        class iterator{
+        class const_iterator{
             private:
             T it;
 
             public:
-            iterator(T iter = nullptr): it(iter) {}
-            //iterator(const iterator &other):it(other.it){}
+            const_iterator(T iter = nullptr): it(iter) {}
 
-            T operator*()const{
+            const T operator*()const{
                 return  it;
             }
             //++i prefix
-            iterator& operator++(){
+            const_iterator& operator++(){
                 it++;
                 return *this;
             }
             //i++ postfix
-            const iterator operator ++(int){
-                iterator temp =*this;
+            const const_iterator operator ++(int){
+                const_iterator temp =*this;
                 it++;
                 return temp;
             }
-            bool operator==(const iterator& other){
+            bool operator==(const const_iterator& other) const {
                 return it==other.it;
             }
-            bool operator!=(const iterator& other){
+            bool operator!=(const const_iterator& other) const {
                 return it!=other.it; 
             }
-            
-            // T* operator-> ()const{
-            //     return &(r_it);
-            // }
-         //   friend ostream& operator<<(ostream& os,const iterator& it);
-
         };//end of iterator class
        
         public:
-        iterator begin()const{
-         return iterator{r_begin};
+        const_iterator begin() const{
+         return const_iterator(r_begin);
         }
-        iterator end()const{
-            return iterator{r_end};
+        const_iterator end() const{
+            return const_iterator(r_end);
         }
-    };   
-        
-}//;
+    };
+}
 
